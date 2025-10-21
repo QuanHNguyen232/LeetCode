54c17863c4594be14ef991f6f0db92fd59b4fcb8
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-10-21 13:12:05 -0400

    LC-adjacent-increasing-subarrays-detection-i [Runtime: 79 ms; Memory: 18 MB]

--- array/adjacent-increasing-subarrays-detection-i.py
+++ array/adjacent-increasing-subarrays-detection-i.py
@@ -1,18 +1,14 @@
 class Solution:
     def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
-        n = len(nums)
-        
-        ans = False
-        sub_arr_idx = set()
-        cnt_incr = 1
-        for i in range(1, n):
-            if nums[i-1] < nums[i]:
-                cnt_incr += 1
-                if cnt_incr >= k:
-                    sub_arr_idx.add(i)
-                    if i-k in sub_arr_idx:
-                        return True
-            else:
-                cnt_incr = 1
+        prev, curr = 0, 1
 
-        return ans
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                curr += 1
+            else:
+                prev = curr
+                curr = 1
+
+            if curr >= k and prev >= k or curr // 2 >= k:
+                return True
+        return False
