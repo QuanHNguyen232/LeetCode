092dd078169e7f6630a9b0b4d8ba82a9db64d93c
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-08 17:41:37 -0500

    LC-two-sum-ii-input-array-is-sorted [Runtime: 767 ms; Memory: 18.7 MB]

--- binary-search/two-sum-ii-input-array-is-sorted.py
+++ binary-search/two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,13 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         from bisect import bisect_left
+        n = len(numbers)
         for num1_idx, num1 in enumerate(numbers):
             num2 = target - num1
             num2_idx = bisect_left(numbers[num1_idx+1:], num2) + (num1_idx+1)
+            if num2_idx >= n: # that number > last number
+                continue
             if numbers[num2_idx] == num2:
                 return [num1_idx+1, num2_idx+1]
+
+        return ans
