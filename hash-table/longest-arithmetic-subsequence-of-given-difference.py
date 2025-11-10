1e13bb0f2f836b0ca98aa03049e144c0afd38a24
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-10 13:02:04 -0500

    LC-longest-arithmetic-subsequence-of-given-difference [Runtime: N/A; Memory: N/A]

---
+++ hash-table/longest-arithmetic-subsequence-of-given-difference.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+        """
+        dp[i]: len of longest subseq has diff=difference unitl i-th number
+        dp[i]: (
+            for j from 0->i
+            max( dp[j] if nums[i]-nums[j]==diff )
+        )
+        """
+        n = len(arr)
+        dp = [1]*n
+
+        for i in range(n):
+            for j in range(i):
+                if arr[i]-arr[j] == difference:
+                    dp[i] = max(dp[i], dp[j]) + 1
+
+        return max(dp)
