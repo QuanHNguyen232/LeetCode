ef09bcd4907706a45f243e099997ffd30fc79e12
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-09 20:11:14 -0500

    LC-best-time-to-buy-and-sell-stock-ii [Runtime: 3 ms; Memory: 19 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock-ii.py
+++ dynamic-programming/best-time-to-buy-and-sell-stock-ii.py
@@ -1,5 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        """
+        dp[i]: max profit at day i-th
+        dp[i] = 
+        """
+        n = len(prices)
+        dp = [0] * n
+        buy = prices[0]
+        dp[0] = 0
+        for i in range(1, n):
+            if buy <= prices[i]:
+                dp[i] = prices[i] - buy
+
+            dp[i] += dp[i-1]
+            buy = prices[i]
+
+        return dp[-1]
+
+    def sln_1(self, prices: List[int]) -> int:
         ans = 0
         for i in range(1, len(prices)):
             if prices[i] > prices[i - 1]:
