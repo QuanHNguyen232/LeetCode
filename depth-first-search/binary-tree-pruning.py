ae716c88747d4e14c8d3f8f6e8c40f9c194de83b
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-11 20:40:55 -0500

    LC-binary-tree-pruning [Runtime: 0 ms; Memory: 17.9 MB]

---
+++ depth-first-search/binary-tree-pruning.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # base case
+        if not root:
+            return root
+
+        # recursion
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+
+        if not (root.left or root.right) and root.val==0:
+            return None
+        else:
+            return root
