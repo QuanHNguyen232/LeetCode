3b8b21bfb23b78311f3258bad81d7592e9a0f3c4
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-14 22:06:19 -0500

    LC-smallest-range-covering-elements-from-k-lists [Runtime: 282 ms; Memory: 49 MB]

---
+++ sliding-window/smallest-range-covering-elements-from-k-lists.py
@@ -0,0 +1,29 @@
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        merged = []
+        for i in range(len(nums)):
+            for num in nums[i]:
+                merged.append((num, i))
+        merged.sort()
+
+        freq = defaultdict(int)
+        left = 0
+        range_start, range_end = 0, math.inf
+        for right in range(len(merged)):
+            right_num, right_idx = merged[right]
+            freq[right_idx] += 1
+
+            while len(freq) >= len(nums):
+                left_num, left_idx = merged[left]
+
+                if right_num - left_num < range_end - range_start:
+                    range_start = merged[left][0]
+                    range_end = merged[right][0]
+
+                freq[left_idx] -= 1
+                if freq[left_idx] == 0:
+                    del freq[left_idx]
+
+                left += 1
+           
+        return [range_start, range_end]
