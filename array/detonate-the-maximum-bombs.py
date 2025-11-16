e9ab72d4ab5d509b49793460aa6046d1bea86dd8
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-15 22:03:29 -0500

    LC-detonate-the-maximum-bombs [Runtime: N/A; Memory: N/A]

---
+++ array/detonate-the-maximum-bombs.py
@@ -0,0 +1,40 @@
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        # bfs
+        visited = set()
+        
+        def get_distance(x1,y1, x2,y2):
+            return sqrt((x1-x2)**2 + (y1-y2)**2)
+        
+        graph = defaultdict(list)
+        for i, bomb1 in enumerate(bombs):
+            for j, bomb2 in enumerate(bombs):
+                if i==j: continue
+                distance = get_distance(bomb1[0],bomb1[1], bomb2[0],bomb2[1])
+                if distance <= bomb1[2]:
+                    graph[i].append(j)
+                    graph[j].append(i)
+
+        def bfs(i):
+            queue = deque([i])
+            cnt = 0
+            while queue:
+                cur_bomb = queue.popleft()
+
+                if cur_bomb in visited: continue
+                visited.add(cur_bomb)
+                cnt += 1
+
+                for next_bomb in graph[cur_bomb]:
+                    if next_bomb in visited: continue
+                    queue.append(next_bomb)
+
+            return cnt
+
+        ans = 1
+        for i, bomb1 in enumerate(bombs):
+            if i in visited: continue
+            num_bombs = bfs(i)
+            ans = max(ans, num_bombs)
+
+        return ans
