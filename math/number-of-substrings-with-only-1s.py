81f0649b46762a2d707cab629efc556ec2147d1e
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-16 14:48:08 -0500

    LC-number-of-substrings-with-only-1s [Runtime: 19 ms; Memory: 18.3 MB]

--- math/number-of-substrings-with-only-1s.py
+++ math/number-of-substrings-with-only-1s.py
@@ -1,16 +1,37 @@
 class Solution:
     def numSub(self, s: str) -> int:
-        return self.count1(s)
+        # return self.count1(s)
+        return self.count2(s)
 
     def count1(self, s:str)->int:
+        """Math-based
+        """
         ans = 0
         MOD = 1e9+7
+
         for ones in s.split("0"):
             L = len(ones)
-            ans += L*(L+1)//2
-            ans %= MOD
+            ans = (ans + L*(L+1)//2) % MOD
         
         return int(ans)
     
     def count2(self, s:str)->int:
-        pass
+        """Count-based
+        """
+        ans = 0
+        MOD = 1e9 + 7
+        curr_cnt = curr_consecutive_ones = 0
+
+        for i in range(len(s)):
+            curr = s[i]
+            if curr == "1":
+                curr_consecutive_ones += 1
+                curr_cnt += curr_consecutive_ones
+            else: # if see "0"
+                ans = (ans + curr_cnt) % MOD
+                curr_cnt = curr_consecutive_ones = 0
+
+        # if last curr="1" --> must add the rest to ans
+        ans = (ans + curr_cnt) % MOD
+
+        return int(ans)
