5c1a046fdff4225368a4376192113e70977a40f3
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-15 19:44:44 -0500

    LC-delete-node-in-a-bst [Runtime: 0 ms; Memory: 21.4 MB]

--- binary-search-tree/delete-node-in-a-bst.py
+++ binary-search-tree/delete-node-in-a-bst.py
@@ -1,93 +1,40 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        """
-               dummy
-              /
-             0
-            / \
-          -2   2
-          /\   /\
-        -3 -1 1  3
-        """
-        dummy = TreeNode(val=math.inf, left=root)
+    # One step right and then always left
+    def successor(self, root: TreeNode) -> int:
+            root = root.right
+            while root.left:
+                root = root.left
+            return root.val
         
-        def find_rm_node(root, key) -> [TreeNode, TreeNode]:
-            prev = dummy
-            curr = root
-            while curr:
-                if curr.val == key:
-                    print(f'find: prev={prev.val if prev else prev}, curr={curr.val if curr else curr}')
-                    return prev, curr
-                elif key < curr.val:
-                    prev = curr
-                    curr = curr.left
-                else:
-                    prev = curr
-                    curr = curr.right
-            return (None, None)
+    # One step left and then always right
+    def predecessor(self, root: TreeNode) -> int:
+        root = root.left
+        while root.right:
+            root = root.right
+        return root.val
 
-        def find_replace_node(node: TreeNode, is_right=True) -> [TreeNode]:
-            prev = node
-            if is_right:
-                if node.left:
-                    curr = node.left
-                else:
-                    curr = node.right
-            else:
-                if node.right:
-                    curr = node.right
-                else:
-                    curr = node.left
-            while curr:
-                if is_right and curr.right:
-                    prev = curr
-                    curr = curr.right
-                elif not is_right and curr.left:
-                    prev = curr
-                    curr = curr.left
-                else:
-                    break
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return None
 
-            # rm connect bw prev & curr
-            if curr == prev.left:
-                prev.left = curr.left if curr else None
-            else:
-                prev.right = curr.right if curr else None
-            print(f'replace: prev={prev.val if prev else prev}, curr={curr.val if curr else curr}')
-            return curr
-
-        prev_node, rm_node = find_rm_node(root, key)
-
-        if rm_node is None:
-            return root
-        
-        # find replace
-        if prev_node.left == rm_node:
-            replace_node = find_replace_node(rm_node, is_right=True)
+        # delete from the right subtree
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        # delete from the left subtree
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        # delete the current node
         else:
-            replace_node = find_replace_node(rm_node, is_right=False)
-
-        # connect new node
-        if prev_node.left == rm_node:
-            prev_node.left = replace_node
-        else:
-            prev_node.right = replace_node
-        
-        # print("prev_node", prev_node)
-        # print("replace_node", replace_node)
-        
-        if replace_node is not None:
-            # update new node's left and right
-            replace_node.left = rm_node.left
-            replace_node.right = rm_node.right
-        
-        # rm node
-        rm_node.left = rm_node.right = None
-
-        return dummy.left
+            # the node is a leaf
+            if not (root.left or root.right):
+                root = None
+            # The node is not a leaf and has a right child
+            elif root.right:
+                root.val = self.successor(root)
+                root.right = self.deleteNode(root.right, root.val)
+            # the node is not a leaf, has no right child, and has a left child    
+            else:
+                root.val = self.predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)
+                        
+        return root
