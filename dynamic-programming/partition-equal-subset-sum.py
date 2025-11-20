44b5e0a2adef306a3f88ebfc826e7034e71cc925
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-20 11:19:41 -0500

    LC-partition-equal-subset-sum [Runtime: N/A; Memory: N/A]

--- dynamic-programming/partition-equal-subset-sum.py
+++ dynamic-programming/partition-equal-subset-sum.py
@@ -1,5 +1,9 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        # return self.dp_1d(nums)
+        return self.dp_2d(nums)
+
+    def dp_1d(self, nums: List[int]) -> bool:
         """
         goal: find subset that sums to subset_sum
         Similar to https://leetcode.com/problems/coin-change-ii/
@@ -22,3 +26,33 @@ class Solution:
                 dp[j] = dp[j] or dp[j - num]
 
         return dp[subset_sum]
+    
+    def dp_2d(self, nums: List[int]) -> bool:
+        """0/1 Knapsack
+        target_sum = total_sum // 2
+        dp[i][j]: can sum, items [0:i] (inclusive), sum = j
+        dp[i][j] = (
+            if not take: dp[i-1][j]
+            if take: dp[i-1][j - num] if j - num >= 0
+        )
+        0<= i < len(nums)
+        0<= j <= sum
+        """
+        total_sum = sum(nums)
+
+        if total_sum % 2 != 0:
+            return False
+        
+        target_sum = total_sum // 2
+        n = len(nums)
+        dp = [[False]*(target_sum+1) for _ in range(n)]
+        # base case
+        dp[0][nums[0]] = True
+
+        # DP
+        for i in range(1, n):
+            for j in range(target_sum+1):
+                not_take = dp[i-1][j]
+                take = dp[i-1][j - nums[i]] if j - nums[i] >= 0 else False
+                dp[i][j] = not_take or take
+        return dp[n-1][target_sum]
