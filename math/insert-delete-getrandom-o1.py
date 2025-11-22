a1a0874334c96ed585b6219a0e55299a05f6166d
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-22 15:09:19 -0500

    LC-insert-delete-getrandom-o1 [Runtime: N/A; Memory: N/A]

---
+++ math/insert-delete-getrandom-o1.py
@@ -0,0 +1,37 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.dict = {}
+        self.list = []
+
+    def insert(self, val: int) -> bool:
+        if val not in self.dict:
+            self.dict[val] = len(self.list)
+            self.list.append(val)
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        if val not in self.dict:
+            return False
+        # update list
+        rm_idx = self.dict[val]
+        rplace_idx = self.dict[self.list[-1]]
+        self.list[rm_idx], self.list[rplace_idx] = self.list[rplace_idx], self.list[rm_idx]
+        # update dict
+        self.dict[self.list[-1]] = rm_idx
+        
+        # delete
+        self.list.pop()
+        del self.dict[val]
+        return True
+
+    def getRandom(self) -> int:
+        return choice(self.list)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
