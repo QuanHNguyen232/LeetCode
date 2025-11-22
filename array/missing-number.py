94cb93df035d37ee01be88d24254d0c22d510b8b
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-21 19:30:37 -0500

    LC-missing-number [Runtime: N/A; Memory: N/A]

---
+++ array/missing-number.py
@@ -0,0 +1,77 @@
+class Solution:
+    def missingNumber(self, nums):
+        # return self.bit(nums)
+        assert self.bsearch1(nums) == self.bsearch2(nums)
+        return self.bsearch1(nums)
+
+    def bsearch2(self, nums):
+        """
+        [0, 1, 2, 3] (index)
+        [0, 1, 2, 3]
+         l  m     r
+               lm r
+                  lr-> condition=False
+                  r  l -> out of loop
+        
+        [0, 1, 2, 3] (index)
+        [0, 1, 2, 4]
+         l  m     r
+               lm r
+                  lrm -> condition=True
+               r  l -> out of loop
+        """
+        def condition(mid):
+            return nums[mid]-nums[0] != mid
+
+        nums.sort()
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if condition(mid):
+                right = mid - 1  # search better result on the left side
+            else:
+                left = mid + 1  # search result on the right side
+
+        return left
+
+
+    def bsearch1(self, nums):
+        """
+        [0,1,2,3] (index)
+        [0,1,2,3]
+         l m   r
+             l r
+               lr -> out of loop
+        """
+        def condition(mid):
+            return nums[mid]-nums[0] != mid
+
+        nums.sort()
+        left = 0
+        right = len(nums)-1
+        
+        while left < right:
+            mid = left + (right - left) // 2 # lower bound
+            if condition(mid):
+                right = mid
+            else:
+                left = mid + 1 # otherwise causes infinite loop
+        if left == nums[left]:
+            return left+1
+        return left
+
+
+    def bit(self, nums):
+        """
+        missing
+            =4 ∧ (0∧0) ∧ (1∧1) ∧ (2∧3) ∧ (3∧4)
+            =(4∧4) ∧ (0∧0) ∧ (1∧1) ∧ (3∧3) ∧ 2
+            =0 ∧ 0 ∧ 0 ∧ 0 ∧ 2
+            =2
+        """
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^= i ^ num
+        
+        return missing
