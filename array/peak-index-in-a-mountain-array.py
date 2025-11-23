073f91f968a94dbf2e2ad1b111abf65e2f3c39ac
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-23 11:24:45 -0500

    LC-peak-index-in-a-mountain-array [Runtime: 0 ms; Memory: 29.6 MB]

--- array/peak-index-in-a-mountain-array.py
+++ array/peak-index-in-a-mountain-array.py
@@ -2,33 +2,39 @@ class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         """
         [0, 1, 2, 3, 4, 5]
-        [0, 5, 6, 9, 2, 1]
+        [5, 9, 6, 3, 2, 0]
          l     m        r
-                  l  m  r
-                  lrm
-                  r  l --> l-1
+         l  m  r
+               lr
 
         [0, 1, 2, 3, 4, 5]
-        [0, 9, 6, 5, 2, 1]
+        [1, 2, 5, 6, 9, 2]
          l     m        r
-         lm r
-            lrm
-            r  l
-        
+                  l  m  r
+                        lr
+
         [0, 1, 2, 3, 4, 5]
         [3, 4, 5, 2, 1, 0]
          l     m        r
                   l  m  r
-                  lrm
-               r  l
+                  lr
         """
         l = 1
         r = len(arr) - 1
-        while l <= r:
+        while l < r:
             mid = l + (r-l)//2
-            if arr[mid-1] < arr[mid]:
-                l = mid + 1
+            if arr[mid-1] >= arr[mid]:
+                r = mid
             else:
-                r = mid - 1
+                l = mid + 1
 
         return l-1
+
+        # while l <= r:
+        #     mid = l + (r-l)//2
+        #     if arr[mid-1] < arr[mid]:
+        #         l = mid + 1
+        #     else:
+        #         r = mid - 1
+
+        # return l-1
