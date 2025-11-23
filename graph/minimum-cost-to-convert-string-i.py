0f792b3546e903d21ce73c66d652d2bf623ad6e3
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-22 22:31:47 -0500

    LC-minimum-cost-to-convert-string-i [Runtime: N/A; Memory: N/A]

---
+++ graph/minimum-cost-to-convert-string-i.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+        graph = defaultdict(list)
+        for src, dst, c in zip(original, changed, cost):
+            graph[src].append((c, dst))
+
+        def dijkstra(src: str, dst: str) -> int:
+            """
+            return min cost (int) to convert from src -> dst
+            """
+            heap = []
+            heapq.heappush(heap, (0, src))
+            visited = set()
+
+            while heap:
+                curr_cost, node = heapq.heappop(heap)
+
+                if node in visited: continue
+                visited.add(node)
+                if node == dst:
+                    return curr_cost
+
+                for next_cost, neighbor in graph[node]:
+                    if neighbor not in visited:
+                        heapq.heappush(heap, (curr_cost + next_cost, neighbor))
+
+            return -1 # cannot find path
+
+        n = len(source)
+        ans = 0
+        for i in range(n):
+            if source[i] == target[i]: continue
+            # find shortest path to convert char from source[i] -> target[i]
+            min_cost = dijkstra(source[i], target[i])
+            if min_cost == -1:
+                return -1
+            ans += min_cost
+        
+        return ans
