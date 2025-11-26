f52934d4820a0493d13d7a4eb96e5d5202ba4272
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-26 10:00:41 -0500

    LC-remove-all-adjacent-duplicates-in-string-ii [Runtime: 43 ms; Memory: 21.9 MB]

--- stack/remove-all-adjacent-duplicates-in-string-ii.py
+++ stack/remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,48 +1,15 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        """
-        deeedbbcccbdaa
-        [d]vs[e]
-            tmp=[e]
-            tmp[-1]!=stack[-1] -> add tmp to stack=[d,e]
-        [d,e]vs[e]
-            tmp=[e]
-            if stack and tmp: tmp=[-1]==stack[-1] -> stack=[d], tmp=[e,e] < k -> do nothing
-            if stack and tmp: tmp=[-1]!=stack[-1] -> stop
-            add tmp to stack=[d,e,e]
-        [d,e,e]vs[e]
-            tmp=[e]
-            if stack and tmp: tmp=[-1]==stack[-1] -> stack=[d,e], tmp=[e,e] < k -> do nothing
-            if stack and tmp: tmp=[-1]==stack[-1] -> stack=[d], tmp=[e,e,e] == k -> tmp=[]
-            not tmp -> stop
-            add tmp to stack=[d]
-        [d]vs[d]
-            tmp=[d]
-            if stack and tmp: tmp=[-1]==stack[-1] -> stack=[], tmp=[d,d] < k -> do nothing
-            not stack -> stop
-            add tmp to stack=[d,d]
-        [d,d]vs[b]
-            tmp=[b]
-            if stack and tmp: tmp=[-1]!=stack[-1] -> stop
-            add tmp to stack=[d,d,b]
-        [d,d,b]vs[b]
-            ...
-        [d,d,b,b]vs[b]
-            tmp=[b]
-            if stack and tmp and tmp=[-1]==stack[-1] -> stack=[d,d,b], tmp=[b,b] < k -> do nothing
-            if stack and tmp and tmp=[-1]==stack[-1] -> stack=[d,d], tmp=[b,b,b]==k -> tmp=[]
-            no tmp -> stop
-            add tmp to stack=[d,d]
-        """
         stack = deque()
-        for char in s:
-            tmp = [char]
-            
-            while stack and tmp and tmp[-1]==stack[-1]:
-                tmp.append(stack.pop())
-                if len(tmp) == k:
-                    tmp = []
-            for ele in tmp:
-                stack.append(ele)
+        for i in s:
+            if stack and i == stack[-1][0]:
+                # update the count
+                stack[-1][1] += 1
+                
+                # rm immediately if count>=k
+                if stack[-1][1] >= k:
+                    stack.pop()
+            else:
+                stack.append([i, 1])
 
-        return "".join(stack)
+        return "".join([char*cnt for char, cnt in stack])
