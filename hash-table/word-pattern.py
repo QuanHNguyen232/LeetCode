c249fd9fb6a79cb8e93b2dc68b8e0d0fb55cea53
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-26 13:24:03 -0500

    LC-word-pattern [Runtime: 0 ms; Memory: 17.7 MB]

--- hash-table/word-pattern.py
+++ hash-table/word-pattern.py
@@ -4,9 +4,20 @@ class Solution:
         if len(pattern) != len(words): return False
 
         pattern_map = {}
+        s_map = {}
         for char, word in zip(pattern, words):
-            if char not in pattern_map:
-                pattern_map[char] = word
-            if char in pattern_map and pattern_map[char] != word:
+            if (
+                char in pattern_map and word not in s_map
+                or char not in pattern_map and word in s_map
+            ):
+                # either in map -> False
                 return False
+            elif char not in pattern_map and word not in s_map:
+                # both not in map -> add
+                pattern_map[char] = word
+                s_map[word] = char
+            else:
+                # both in map -> check
+                if pattern_map[char] != word or s_map[word] != char:
+                    return False
         return True
