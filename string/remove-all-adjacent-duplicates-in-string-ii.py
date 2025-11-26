a71d1dfbb55d78481bb212a6ce1e90e70500fa3c
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-26 10:07:11 -0500

    LC-remove-all-adjacent-duplicates-in-string-ii [Runtime: 44 ms; Memory: 22 MB]

--- string/remove-all-adjacent-duplicates-in-string-ii.py
+++ string/remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
         stack = deque()
-        for i in s:
+        for i in s: # O(n)
             if stack and i == stack[-1][0]:
                 # update the count
                 stack[-1][1] += 1
@@ -13,3 +13,18 @@ class Solution:
                 stack.append([i, 1])
 
         return "".join([char*cnt for char, cnt in stack])
+    
+    def time_limit_exceed(self, s: str, k: int) -> str:
+        stack = deque()
+        for char in s: #O(n)
+            tmp = [char]
+            
+            while stack and tmp and tmp[-1]==stack[-1]: # O(k)
+                tmp.append(stack.pop())
+                if len(tmp) == k:
+                    tmp = []
+            for ele in tmp:
+                stack.append(ele)
+
+        # TOTAL: O(n*k)
+        return "".join(stack)
