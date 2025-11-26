d41b22fa7173d4a46770bca98f8c3a3f6e6941a9
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-26 10:51:22 -0500

    LC-design-search-autocomplete-system [Runtime: N/A; Memory: N/A]

---
+++ data-stream/design-search-autocomplete-system.py
@@ -0,0 +1,85 @@
+class TrieNode:
+    def __init__(self):
+        self.child = {}
+        self.countEnd = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word, freq):
+        curr = self.root
+        for c in word:
+            if c not in curr.child:
+                curr.child[c] = TrieNode()
+            curr = curr.child[c]
+        curr.countEnd = freq
+
+    def search(self, keyword: Union[List[str], str]) -> List[Tuple[str, int]]:
+        curr = self.root
+        for c in keyword:
+            if c not in curr.child:
+                return []
+            curr = curr.child[c]
+        
+        ans = []
+        # self.dfs(curr)
+        self.backtrack(ans, curr, path=[])
+
+        return ans
+    
+    def backtrack(self, ans, curr: TrieNode, path: List[str]):
+        # base
+        if curr.countEnd:
+            ans.append((curr.countEnd, ''.join(path)))
+        # recursion
+        for next_char in curr.child:
+            self.backtrack(ans, curr.child[next_char], path+[next_char])
+
+    # def dfs(self, curr)-> List[str]:
+    #     ans = []
+    #     # backtrack
+    #     def backtrack(ans, curr, path: List[str]):
+    #         # base
+    #         if curr.countEnd:
+    #             ans.append((curr.countEnd, ''.join(path)))
+    #         # recursion
+    #         for next_char in curr.child:
+    #             backtrack(ans, curr.child[next_char], path+[next_char])
+    #     backtrack(curr, [])
+    #     return ans
+    
+    def __str__(self):
+        ans = self.search("")
+        return '\n'.join(f"cnt:{cnt}, s={s}" for s, cnt in ans)
+
+class AutocompleteSystem:
+
+    def __init__(self, sentences: List[str], times: List[int]):
+        self.LIMIT = 3
+        self.curr_search = []
+        self.trie = Trie()
+        for sent, freq in zip(sentences, times):
+            self.trie.insert(sent, freq)
+
+        # print(self.trie)
+
+    def input(self, c: str) -> List[str]:
+        if c == "#":
+            curr_sentence = "".join(self.curr_search)
+            self.trie.insert(curr_sentence, 1)
+            self.curr_search = []
+            return []
+        
+        self.curr_search.append(c)
+        keyword = "".join(self.curr_search)
+        matches = self.trie.search(keyword)
+        matches.sort(key=lambda x: [-x[0], x[1]])
+        # print(self.curr_search, matches)
+        ans = matches[:self.LIMIT]
+        return [keyword+sent for cnt, sent in ans]
+
+
+# Your AutocompleteSystem object will be instantiated and called as such:
+# obj = AutocompleteSystem(sentences, times)
+# param_1 = obj.input(c)
