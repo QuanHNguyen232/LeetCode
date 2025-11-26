85fd35c4f52e34a63c237b0f04d4fd3f36724904
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-26 08:31:48 -0500

    LC-implement-magic-dictionary [Runtime: 131 ms; Memory: 20.5 MB]

--- hash-table/implement-magic-dictionary.py
+++ hash-table/implement-magic-dictionary.py
@@ -1,25 +1,35 @@
 class MagicDictionary:
 
     def __init__(self):
-        self.original_dict = set()
-        self.vocab_dict = defaultdict(set)
+        self.word_set = set()
+        self.counter = Counter()
 
     def generate_masked_word(self, word: str) -> List[str]:
         masks = [word[:i] + "#" + word[i+1:] for i in range(len(word))]
         return masks
-    def buildDict(self, dictionary: List[str]) -> None:
-        for word in dictionary:
-            self.original_dict.add(word)
-            for masked_word in self.generate_masked_word(word):
-                self.vocab_dict[len(masked_word)].add(masked_word)
-        
-        print(self.vocab_dict)
 
+    def buildDict(self, dictionary: List[str]) -> None:
+        self.word_set = self.word_set.union(set(dictionary))
+        self.counter.update(Counter(nei for word in dictionary for nei in self.generate_masked_word(word)))
+        
     def search(self, searchWord: str) -> bool:
-        if searchWord in self.original_dict:
-            return False
-        masked_searchWords = self.generate_masked_word(searchWord)
-        return any([masked_searchWord in self.vocab_dict[len(masked_searchWord)] for masked_searchWord in masked_searchWords])
+        """
+        e.g. 
+        if count[h#llo]=2 (from "hallo" and "hello")
+        --> searchWord="hello" -> True as it can be generated from "hallo"
+        
+        if count[h#llo]=1 (from "hello" only)
+        --> searchWord="hello" (in word_set) -> False as it can not be itself
+        --> searchWord="hallo" (not in word_set) -> True
+        """
+        for nei in self.generate_masked_word(searchWord):
+            if (
+                (self.counter[nei] > 1)
+                or (self.counter[nei] == 1 and searchWord not in self.word_set)
+            ):
+                return True
+
+        return False
 
 
 # Your MagicDictionary object will be instantiated and called as such:
