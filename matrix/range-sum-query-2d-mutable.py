4844cfbeeb36a294fc7d4ded273f04e3b222e9a0
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-11-26 11:28:28 -0500

    LC-range-sum-query-2d-mutable [Runtime: N/A; Memory: N/A]

--- matrix/range-sum-query-2d-mutable.py
+++ matrix/range-sum-query-2d-mutable.py
@@ -3,21 +3,30 @@ class NumMatrix:
     def __init__(self, matrix: List[List[int]]):
         self.nrows = len(matrix)
         self.ncols = len(matrix)
-        self.matrix = matrix
-        for row in self.matrix: print(row)
-        print("-"*20)
-        for i, row in enumerate(self.matrix):
-            self.matrix[i] = list(itertools.accumulate(row))
-        
-        for r in range(self.n)
-        
-        for row in self.matrix: print(row)
+        self.original = matrix
+        self.prefix = deepcopy(matrix)
 
+        for i, row in enumerate(self.prefix):
+            self.prefix[i] = list(itertools.accumulate(row))
+        
+        for r in range(1, self.nrows):
+            for c in range(0, self.ncols):
+                self.prefix[r][c] += self.prefix[r-1][c]
+        
     def update(self, row: int, col: int, val: int) -> None:
-        pass
+        diff = val - self.original[row][col]
+        self.original[row][col] = val
+
+        for r in range(row, self.nrows):
+            for c in range(col, self.ncols):
+                self.prefix[r][c] += diff
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return 0
+        ans = self.prefix[row2][col2]
+        ans -= self.prefix[row1-1][col2] if row1>0 else 0
+        ans -= self.prefix[row2][col1-1] if col1>0 else 0
+        ans += self.prefix[row1-1][col1-1] if (row1>0 and col1>0) else 0
+        return ans
 
 
 # Your NumMatrix object will be instantiated and called as such:
