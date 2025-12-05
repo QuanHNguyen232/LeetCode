f138b3d8f51bb4d5b6c635d7d79dcd293c188dc0
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-12-05 00:25:37 -0500

    LC-online-stock-span [Runtime: 74 ms; Memory: 22.9 MB]

--- design/online-stock-span.py
+++ design/online-stock-span.py
@@ -1,20 +1,15 @@
 class StockSpanner:
 
     def __init__(self):
-        # store value and index
-        self.idx = 0
+        # store value and index in Monotonic stack
         self.max_stack = []
 
     def next(self, price: int) -> int:
-        self.idx += 1
         ans = 1
-        # print(self.max_stack, (price, self.idx))
         while self.max_stack and self.max_stack[-1][0] <= price:
-            prev_price, prev_idx = self.max_stack.pop()
-        if self.max_stack:
-            ans = max(ans, self.idx - self.max_stack[-1][1])
-        self.max_stack.append([price, self.idx])
-        # print(ans)
+            ans += self.max_stack.pop()[1]
+
+        self.max_stack.append([price, ans]) # price span of price
         return ans
 
 
