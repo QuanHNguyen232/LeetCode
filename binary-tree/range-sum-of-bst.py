5a90acbddc4f1619267038286ea3a5f1b5f92586
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-12-05 22:32:42 -0500

    LC-range-sum-of-bst [Runtime: 3 ms; Memory: 25.1 MB]

---
+++ binary-tree/range-sum-of-bst.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if not root: return 0
+        
+        total = 0
+        if low <= root.val <= high:
+            total += root.val
+            total += self.rangeSumBST(root.right, low, high)
+            total += self.rangeSumBST(root.left, low, high)
+        elif root.val < low:
+            total += self.rangeSumBST(root.right, low, high)
+        else:
+            total += self.rangeSumBST(root.left, low, high)
+            
+        return total
