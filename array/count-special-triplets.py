adb32cd1e6072c915ab3a770b8550ca613937d2f
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-12-08 23:00:46 -0500

    LC-count-special-triplets [Runtime: N/A; Memory: N/A]

--- array/count-special-triplets.py
+++ array/count-special-triplets.py
@@ -6,26 +6,18 @@ class Solution:
 
         for i, num in enumerate(nums):
             hashmap[num].append(i)
-        
+
         for num_j in hashmap.keys():
-            num_i = num_k = num_j*2
-            if num_i not in hashmap: continue
+            target = num_j * 2
+            if target not in hashmap: continue
             for idx, j in enumerate(hashmap[num_j]):
-                if j == 0 or j==len(nums)-1: # then there is no i or no k
-                    continue
-                if num_j==num_i:
-                    # print(num_j, j, hashmap[num_i])
-                    if 0 < idx < len(hashmap[num_j]):
-                        cnt_i = idx
-                        cnt_k = len(hashmap[num_j]) - idx - 1
-                        ans = (ans + cnt_i*cnt_k) % MOD
-                else:
-                    # search for idx j in hashmap[num_i]
-                    # find num of idx i (i<j)
-                    # find num of idx k (j<k)
-                    cnt_i = bisect_right(hashmap[num_i], j)
-                    cnt_k = len(hashmap[num_i]) - cnt_i
-                    # print(num_j, j, cnt_i, cnt_k, hashmap[num_i])
-                    ans = (ans + cnt_i*cnt_k) % MOD
+                if j == 0 or j==len(nums)-1: continue # then there is no i (j=0) or no k (j=len(nums)-1)
+                if num_j == target and 0 < idx < len(hashmap[num_j]):
+                    cnt_i = idx
+                    cnt_k = len(hashmap[num_j]) - idx - 1
+                else: # case: num_j != target
+                    cnt_i = bisect_right(hashmap[target], j) # search for idx j in hashmap[target]
+                    cnt_k = len(hashmap[target]) - cnt_i
+                ans = (ans + cnt_i*cnt_k) % MOD
 
         return int(ans)
