8018baff9ac53c557d8d27cac7fb74b967f2ecad
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-12-08 23:02:28 -0500

    LC-count-special-triplets [Runtime: 431 ms; Memory: 47.4 MB]

--- array/count-special-triplets.py
+++ array/count-special-triplets.py
@@ -4,20 +4,23 @@ class Solution:
         MOD = 1e9+7
         ans = 0
 
-        for i, num in enumerate(nums):
+        for i, num in enumerate(nums): # O(n)
             hashmap[num].append(i)
-
-        for num_j in hashmap.keys():
+        
+        for num_j in hashmap.keys(): # O(n)
             target = num_j * 2
             if target not in hashmap: continue
             for idx, j in enumerate(hashmap[num_j]):
+                cnt_i = cnt_k = 0
                 if j == 0 or j==len(nums)-1: continue # then there is no i (j=0) or no k (j=len(nums)-1)
-                if num_j == target and 0 < idx < len(hashmap[num_j]):
-                    cnt_i = idx
-                    cnt_k = len(hashmap[num_j]) - idx - 1
-                else: # case: num_j != target
-                    cnt_i = bisect_right(hashmap[target], j) # search for idx j in hashmap[target]
+                if num_j == target:
+                    if 0 < idx < len(hashmap[num_j]):
+                        cnt_i = idx
+                        cnt_k = len(hashmap[num_j]) - idx - 1
+                else:
+                    cnt_i = bisect_right(hashmap[target], j) # O(logn)
                     cnt_k = len(hashmap[target]) - cnt_i
-                ans = (ans + cnt_i*cnt_k) % MOD
+
+                ans = (ans + cnt_i * cnt_k) % MOD
 
         return int(ans)
