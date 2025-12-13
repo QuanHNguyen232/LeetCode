658e0b04d90d57d35c557bc4410c0deeb7ef2b77
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-12-13 02:17:09 -0500

    LC-snakes-and-ladders [Runtime: 35 ms; Memory: 17.9 MB]

--- matrix/snakes-and-ladders.py
+++ matrix/snakes-and-ladders.py
@@ -2,6 +2,7 @@ class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         # unweighted graph -> bfs
         n = len(board)
+        START_CELL = 1
         NOT_VISITED = -1
         idx2position = {}
         distance = [NOT_VISITED] * (n**2 + 1)
@@ -15,7 +16,7 @@ class Solution:
             if r % 2 != 0:
                 board[r] = board[r][::-1]
 
-        curr_cell = 1
+        curr_cell = START_CELL
         for r in range(n):
             for c in range(n):
                 idx2position[curr_cell] = (r, c)
@@ -23,7 +24,7 @@ class Solution:
 
         def bfs():
             queue = deque()
-            queue.append([1, 0])
+            queue.append([START_CELL, 0])
 
             while queue:
                 curr, curr_dist = queue.popleft()
@@ -34,12 +35,10 @@ class Solution:
                 for next_cell in range(curr + 1, min(curr + 6, n**2) + 1):
                     r, c = idx2position[next_cell]
                     val = board[r][c]
-                    print(f"\tnext_cell={next_cell}, val={val}")
-                    if val != -1:
-                        queue.append([val, curr_dist + 1])
-                    else:
-                        queue.append([next_cell, curr_dist + 1])
-                    print(f"\t\tgo to={queue[-1][0]}, cost={queue[-1][1]}")
+                    # if ladder/snake -> go there without count step
+                    # e.g: curr=1, but next=2 has ladder to 15 ==> cost 1 step for 1 -> 15, instad of 2 steps (1 -> 2 -> 15)
+                    next_cell = val if val != -1 else next_cell
+                    queue.append([next_cell, curr_dist + 1])
 
         bfs()
         return distance[-1]
