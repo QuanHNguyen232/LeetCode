8144035f1a38cd31047c8d393daf9ad6db51e07c
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-12-13 01:13:11 -0500

    LC-count-sub-islands [Runtime: 1673 ms; Memory: 75.9 MB]

--- depth-first-search/count-sub-islands.py
+++ depth-first-search/count-sub-islands.py
@@ -1,8 +1,6 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         # use bfs
-        # check is_subisland by setB.issubset(setA) -> 
-        # 
         count = 0
         nrows = len(grid1)
         ncols = len(grid1[0])
@@ -12,7 +10,7 @@ class Solution:
         def is_in_bound(r, c):
             return (0 <= r < nrows) and (0 <= c < ncols)
 
-        def bfs(grid, start_r, start_c, idx: int, id2set: dict) -> set:
+        def bfs(grid, start_r, start_c, idx: int = -1, id2set: dict = None) -> set:
             cell_set = set()
             queue = deque()
             queue.append([start_r, start_c])
@@ -31,7 +29,8 @@ class Solution:
                     ):
                         queue.append([new_r, new_c])
 
-            id2set[idx] = cell_set
+            if id2set is not None:
+                id2set[idx] = cell_set
             return cell_set
 
         # bfs on grid 1 -> store islands
@@ -44,15 +43,14 @@ class Solution:
                     idx -= 1
 
         # bfs on grid 2 -> find if this island is subisland from grid 1
-        idx = -1
         for r in range(nrows):
             for c in range(ncols):
                 if grid2[r][c] == LAND:
-                    curr_island = bfs(grid2, r, c, -1, {})
-                    # check curr_island idx on grid1 using (r, c)
-                    island_idx = grid1[r][c]
+                    island_2 = bfs(grid2, r, c)
+                    # check island_2's idx on grid1 using (r, c)
                     # check is subset (subisland)
-                    if (island_idx in id2set) and curr_island.issubset(id2set[island_idx]):
+                    island_idx = grid1[r][c]
+                    if (island_idx in id2set) and island_2.issubset(id2set[island_idx]):
                         count += 1
 
         return count
