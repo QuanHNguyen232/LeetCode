ca7e60786c1834444b84f6c38b662840d1914cea
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2025-12-14 21:57:41 -0500

    LC-number-of-smooth-descent-periods-of-a-stock [Runtime: 63 ms; Memory: 30 MB]

--- dynamic-programming/number-of-smooth-descent-periods-of-a-stock.py
+++ dynamic-programming/number-of-smooth-descent-periods-of-a-stock.py
@@ -8,17 +8,22 @@ class Solution:
         )
         """
         n = len(prices)
-        dp = [0]*n
-        
+
         # base case
-        dp[0] = 1
+        # can be optimized to use just a variable "ans"
+        # dp = [0]*n
+        ans = 1 # dp[0] = 1
+
         prev_smooth_idx = 0
 
         for i in range(1, n):
             if prices[i] != prices[i-1] - 1:
                 prev_smooth_idx = i
 
-            dp[i] = dp[i-1] + 1 # add current i
-            dp[i] += i - prev_smooth_idx
+            # add current i (itself is valid)
+            ans += 1 # dp[i] = dp[i-1] + 1
 
-        return dp[n-1]
+            # add num of subarr that continuously decrease
+            ans += i - prev_smooth_idx # dp[i] += i - prev_smooth_idx
+
+        return ans #dp[n-1]
