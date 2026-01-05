1c206f94f0b1dcb0201d5f1a784a6f8a91784e04
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2026-01-05 03:19:23 -0500

    LC-split-array-with-equal-sum [Runtime: N/A; Memory: N/A]

--- array/split-array-with-equal-sum.py
+++ array/split-array-with-equal-sum.py
@@ -14,21 +14,27 @@ class Solution:
         total = sum(nums)
         pre_sum = list(itertools.accumulate(nums))
 
-        def can_split_half(pre_sum, start, end):
+        def can_split_half(pre_sum, start, end) -> set:
             """
             start, end: inclusive
+
+            must return all possible cases that can be splitted
+            e.g: [1,2,-1,1,2,5,2,5,2]
+            j = 5
+            -> find 2 cases for i: i=2 (split_val=3) and i=3 (split_val=2)
             """
+            ans = set()
             left_sum = 0
             right_sum = (pre_sum[end] - pre_sum[start-1]) if start > 0 else pre_sum[end]
             # print("\t", (start, end), (left_sum, right_sum))
-            for i in range(start, end):
+            for i in range(start, end+1):
                 right_sum -= nums[i]
                 if i > start:
                     left_sum += nums[i - 1]
                 # print(f"\t\ti={i}, left_sum={left_sum}, right_sum={right_sum}")
                 if left_sum == right_sum:
-                    return i, left_sum
-            return -1, 0
+                    ans.add(left_sum)
+            return ans
         
 
         if n < 7: return False
@@ -41,14 +47,15 @@ class Solution:
             # print(f"j={j}")
 
             # find if can find i that can split 0...j by half
-            idx_i, split_val_i = can_split_half(pre_sum, 0, j-1)
+            split_vals_i = can_split_half(pre_sum, 0, j-1)
             
             # find if can find k that can split j...n by half
-            idx_k, split_val_k = can_split_half(pre_sum, j+1, n-1)
+            split_vals_k = can_split_half(pre_sum, j+1, n-1)
             
-            # print(f"find i={(idx_i, split_val_i)}, k={(idx_k, split_val_k)}")
+            # print(f"find i={split_vals_i}, k={split_vals_k}")
 
-            if idx_i != -1 and idx_k != -1 and split_val_i == split_val_k:
+            if split_vals_i & split_vals_k:
                 return True
 
         return False
+        
