017dac7b10e7ba64d095a80ca9afc5cfcab221f4
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2026-01-05 03:28:06 -0500

    LC-split-array-with-equal-sum [Runtime: 1116 ms; Memory: 17.5 MB]

--- hash-table/split-array-with-equal-sum.py
+++ hash-table/split-array-with-equal-sum.py
@@ -26,37 +26,30 @@ class Solution:
             """
             ans = set()
             left_sum = 0
-            right_sum = (pre_sum[end] - pre_sum[start-1]) if start > 0 else pre_sum[end]
-            # print("\t", (start, end), (left_sum, right_sum))
+            if start > 0:
+                right_sum = pre_sum[end] - pre_sum[start-1]
+            else:
+                right_sum = pre_sum[end]
+
             for i in range(start, end+1):
                 right_sum -= nums[i]
                 if i > start:
                     left_sum += nums[i - 1]
-                # print(f"\t\ti={i}, left_sum={left_sum}, right_sum={right_sum}, idx={i if left_sum == right_sum and start < i < end else None}")
-                if left_sum == right_sum and start < i < end:
+
+                if start < i < end and left_sum == right_sum:
                     ans.add(left_sum)
             return ans
         
 
         if n < 7: return False
         for j in range(n):
-            if not (3 <= j <= n-4):
-                # smallest i = 1 (s.t. can get sum(0, i-1)) and i+1 < j ==> j >= 3
-                # largest k = n-2 (s.t. can get sum(k+1, n-1)) and j + 1 < k < n - 1 ==> j <= n-4
-                continue
-
-            print(f"j={j}")
-
             # find if can find i that can split 0...j by half
             split_vals_i = can_split_half(pre_sum, 0, j-1)
             
             # find if can find k that can split j...n by half
             split_vals_k = can_split_half(pre_sum, j+1, n-1)
             
-            print(f"find i={split_vals_i}, k={split_vals_k}")
-
             if split_vals_i & split_vals_k:
                 return True
 
         return False
-        # [0,-3,10,-10,-8,-7,5,-7,5,-3]
