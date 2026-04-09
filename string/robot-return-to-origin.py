a67b0322ae0414e0631628127837837431d9bffb
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2026-04-08 20:22:06 -0400

    LC-robot-return-to-origin [Runtime: 7 ms; Memory: 19.5 MB]

--- string/robot-return-to-origin.py
+++ string/robot-return-to-origin.py
@@ -1,20 +1,5 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        stack = []
-        for move in list(moves):
-            stack.append(move)
+        cnter = Counter(list(moves))
 
-            while len(stack) >= 2:
-                prev_move, move = stack[-2], stack[-1]
-                if (prev_move == "D" and move == "U"
-                    or prev_move == "U" and move == "D"
-                    or prev_move == "L" and move == "R"
-                    or prev_move == "R" and move == "L"
-                ):
-                    stack.pop()
-                    stack.pop()
-                else:
-                    break
-
-        
-        return len(stack)==0
+        return cnter.get('D')==cnter.get('U') and cnter.get('R')==cnter.get('L')
