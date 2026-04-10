3d32dc10ec984c48d51f20f86ea78ca9040708c7
QuanHNguyen232 <91759678+QuanHNguyen232@users.noreply.github.com>
2026-04-10 01:24:52 -0400

    LC-minimum-distance-between-three-equal-elements-i [Runtime: 0 ms; Memory: 19.4 MB]

---
+++ array/minimum-distance-between-three-equal-elements-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        cnter = defaultdict(list)
+        for i, num in enumerate(nums):
+            cnter[num].append(i)
+        
+        res = float("inf")
+        for num, indices in cnter.items():
+            if len(indices) < 3:
+                continue
+            for idx in range(2, len(indices)):
+                i, j, k = indices[idx-2], indices[idx-1], indices[idx]
+                distance = abs(i-j) + abs(j-k) + abs(k-i)
+                res = min(res, distance)
+
+        return -1 if res==float("inf") else res
